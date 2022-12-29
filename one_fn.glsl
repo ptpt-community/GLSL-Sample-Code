71aa5949c73f6b1ee7db6b6bcfb10214090174a9
Labnan <khalid.masum.92@gmail.com>
2022-12-29 23:55:40 +0600

    Create one_fn.glsl

---
+++ one_fn.glsl
@@ -0,0 +1,39 @@
+precision highp float;
+varying vec3 fNormal;
+varying vec3 worldPos;
+varying vec3 localPos;
+
+vec3 work() {
+  vec3 dir = vec3(0,1,0); // high noon
+
+  vec3 cpos = localPos;
+  vec3 color;
+  
+  int time = 0;
+  
+  if ( time == 0)
+    {
+      color = vec3(1, 3,1); 
+    }
+    else if (time ==4){
+      color = vec3(1, .7,1); 
+       
+      if (time == 5) {
+        color = vec3(1, .45,3); 
+      }
+
+    }
+    return dir;
+
+}
+
+void main()
+{
+  vec3 dir;
+  
+  dir = work();
+  float diffuse = .5 + dot(fNormal,dir);
+  gl_FragColor = vec4(diffuse, color, 1.0);
+  
+  
+}
